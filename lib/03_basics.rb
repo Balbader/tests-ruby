42279e1529449f0eb09c6e37c847d27925dae3aa
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-17 17:18:11 +0100

    need review

---
+++ lib/03_basics.rb
@@ -0,0 +1,43 @@
+def who_is_bigger(a, b, c)
+  if a == nil || b == nil || c == nil
+    return "nil detected"
+  end
+  if a > b && a > c
+    return "a is bigger"
+  elsif b > a && b > c
+    return "b is bigger"
+  else
+    return "c is bigger"
+  end
+end
+
+def reverse_upcase_noLTA(string)
+  str = string.upcase
+  i = 0
+  while str[i]
+    if str[i] == "T"
+      str[i] = ""
+      i += 1
+    end
+    i += 1
+  end
+  j = 0
+  while str[j]
+    if str[j] == "A" || str[j] == "L"
+      str[j] = ""
+      j += 1
+    end
+    j += 1
+  end
+  return str.reverse
+end
+
+def array_42(arr)
+  return arr.include? "42".to_i
+end
+
+=begin
+puts reverse_upcase_noLTA("Tries this at Home, Kids")
+puts reverse_upcase_noLTA("Ponies loves carrots")
+puts reverse_upcase_noLTA("qwertyuiopasdfghjkl;zxcvbn")
+=end
