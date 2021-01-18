7e0231642f6a69615e1bda6924c4686a8e225eca
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-18 21:29:40 +0100

    update

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -30,33 +30,3 @@ def rearrange(word) #rearranges individual words into pig latin
   end
   new_word
 end
-
-puts translate("apple")
-puts translate("banana")<<
-puts translate("cherry")
-puts translate("eat pie")
-puts translate("three")
-puts translate("school")
-puts translate("quiet")
-puts translate("square")
-puts translate("the quick brown fox")
-
-=begin
-def translate(str)
-  vowels = ["a", "e", "i", "o", "u"]
-
-  if vowels.include? str[0]
-    str + "ay"
-  elsif str[0..1] == "qu"
-    str[2..-1] + "quay"
-  elsif str[1..2] == "qu"
-    str[3..-1] + str[0..2] + "ay"
-  elsif !(vowels.include? str[0]) && !(vowels.include? str[1]) && !(vowels.include? str[2])
-    str[3..-1] + str[0..2] + "ay"
-  elsif !(vowels.include? str[0]) && !(vowels.include? str[1])
-    str[2..-1] + str[0..1] + "ay"
-  else
-    str[1..-1] + str[0] + "ay"
-  end
-end
-=end
