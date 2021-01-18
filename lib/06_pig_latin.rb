a4ee4b6b3f84e30602262c0da81adf5bce52c53d
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-18 20:45:08 +0100

    update

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,19 +1,29 @@
 def translate(str)
-
   vowels = ["a", "e", "i", "o", "u"]
 
   if vowels.include? str[0]
-    return str + "ay"
+    str + "ay"
   elsif str[0..1] == "qu"
-    return str[2..-1] + "quay"
+    str[2..-1] + "quay"
   elsif str[1..2] == "qu"
-    return str[3..-1] + str[0..2] + "ay"
+    str[3..-1] + str[0..2] + "ay"
   elsif !(vowels.include? str[0]) && !(vowels.include? str[1]) && !(vowels.include? str[2])
-    return str[3..-1] + str[0..2] + "ay"
-  elsif !(vowels.include? str[0]) && !(vowels.include? str[1]) # for 2
-    return str[2..-1] + str[0..1] + "ay"
+    str[3..-1] + str[0..2] + "ay"
+  elsif !(vowels.include? str[0]) && !(vowels.include? str[1])
+    str[2..-1] + str[0..1] + "ay"
   else
-    return str[1..-1] + str[0] + "ay"
+    str[1..-1] + str[0] + "ay"
   end
 end
+
+puts translate("apple")
+puts translate("banana")
+puts translate("cherry")
+puts translate("eat pie")
+puts translate("three")
+puts translate("school")
+puts translate("quiet")
+puts translate("square")
+puts translate("the quick brown fox")
+
 
