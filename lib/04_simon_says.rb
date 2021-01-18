d4e056517922af23914a37bc1f8765c333b56824
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-18 17:39:13 +0100

    update

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -6,18 +6,27 @@ def shout(str)
   return str.upcase
 end
 
-def repeat(str, nb)
-  nb.times do
-    puts str
+def repeat(str, *p)
+  if str && p == nil
+    print "#{str} "
+    print str
+  elsif str && p
+    i = 0
+    while i < p.length
+      print "#{str} "
+      i += 1
+    end
   end
 end
 
 def start_of_word(str, nb)
   i = 0
+  word = ""
   while i < nb
-    print str[i]
+    word += str[i]
     i += 1
   end
+  return word
 end
 
 def first_word(str)
@@ -27,11 +36,16 @@ end
 def titleize(str)
   puts str
   str = str.capitalize
-  puts str
   str = str.split(" ")
-  print str
-  puts
-  print (str[i].join(" ")).capitalize
+  i = 1
+  while i < str.length
+    if str[i].length <= 3
+      str[i] = str[i]
+      i += 1
+    else
+      str[i] = str[i].capitalize
+      i += 1
+    end
+  end
+  puts str.join(" ")
 end
-
-titleize("the bridge over the river kwai")
